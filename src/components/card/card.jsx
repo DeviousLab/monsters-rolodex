07e1769dc6e99f851e56ead03607cb0731850c7d
DeviousLab <deviouslab@gmail.com>
2021-09-12 10:36:04 +0400

    Added search functionality
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -3,7 +3,7 @@ import './card.css'
 
 export const Card = props => {
     return (
-    <div class='card-container'>
+    <div className='card-container'>
         <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster" />
         <h2>{props.monster.name}</h2>
         <p>{props.monster.email}</p>
