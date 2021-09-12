7a23cd0d6da4685e0bc732aeed41c5867dd1ceb9
DeviousLab <deviouslab@gmail.com>
2021-09-12 09:38:18 +0400

    Added card list component
    Signed-off-by: DeviousLab <deviouslab@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list';
 import './App.css';
+
 
 class App extends Component {
   constructor() {
@@ -18,11 +20,12 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CardList name="Devious">
         {this.state.monsters.map(monster => (
           <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+        ))};
+        </CardList>
       </div>
-    );
-  }
+    )}
 }
 export default App;
